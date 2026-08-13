aaf5e6b7451fd4228f8d9aae4de3f1998a24a240
trendbyak <aktrends4u@gmail.com>
2026-08-13 15:50:16 +0530

    Update Trends by AK website and admin

---
+++ admin/supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://ltxrycmreumofpcbwnbi.supabase.co";
+
+const SUPABASE_KEY = sb_publishable_wdc4ImKB1f0Q-v4Po9DOwA_xIpPXHkh;
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
