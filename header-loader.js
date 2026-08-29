4ba14eeed46a5bd43eb46ea6af81e793bf80ec66
trendbyak <aktrends4u@gmail.com>
2026-08-29 19:04:46 +0530

    Add one-file loader for shared header and styles

---
+++ header-loader.js
@@ -0,0 +1,17 @@
+(function(){
+  var base = document.currentScript ? document.currentScript.src.replace(/\/header-loader\.js(?:\?.*)?$/, '/') : '';
+  if (!base) base = location.origin + '/';
+
+  if (!document.querySelector('link[data-trends-header-css]')) {
+    var css = document.createElement('link');
+    css.rel = 'stylesheet';
+    css.href = base + 'header.css';
+    css.dataset.trendsHeaderCss = '1';
+    document.head.appendChild(css);
+  }
+
+  var script = document.createElement('script');
+  script.src = base + 'header.js';
+  script.defer = true;
+  document.head.appendChild(script);
+})();
